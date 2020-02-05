a444e4fa71201da4863d6cdc25fd105b643cab78
initial-mockingbird <daniel.andres.pinto@gmail.com>
2020-02-05 19:08:18 -0400

    added a lil more documentation

--- lab4/BFS.java
+++ lab4/BFS.java
@@ -1,37 +1,47 @@
 import java.util.*; 
+/**<p>Clase que busca caminos Hamiltonianos usando BFS.</p>
+ */
 public class BFS{
 	
+	/** Dado un grafo, usando BFS encuentra caminos hamiltonianos si hay alguno.
+	*
+	*	@param g Grafo sobre el cual se aplicara bfs para encontrar el camino hamiltoniano.
+	*
+	**/
+
 	public String bfs(Grafo g){
 		ArrayList<ArrayList<Integer>> cadenas = new ArrayList<ArrayList<Integer>>(1);
-		HashSet<Integer> vertices = g.get_vertices();
-		Iterator<Integer> v = vertices.iterator();
-		String res="";
+		Iterator<Integer> vertices = g.get_vertices().iterator();
+		String respuesta="";
 
-		while(v.hasNext()){
+		while(vertices.hasNext()){
 			ArrayList<Integer> raiz = new ArrayList<Integer>();
-			raiz.add(v.next());
+			raiz.add(vertices.next());
 			cadenas.add(raiz);
-			res = bfs(g,cadenas);
-			if (res!="No hay respuesta :("){
-				return res.substring(0,res.lastIndexOf("-")-1);
+			respuesta = bfs(g,cadenas);
+			if (respuesta!="No hay respuesta :("){
+				return respuesta;
 			}
 			cadenas.remove(raiz);
 		}
-
-		return res;
-
+		return respuesta;
 	}
 
+	/** El procedimiento BFS de por si.
+	*
+	*	@param g Grafo sobre el cual se hara bfs/
+	*	@param cadenas lista de todas las cadenas de largo a lo sumo <code>i</code>
+	*	que se desprenden del nodo raiz (el primer elemento de todas las cadenas).
+	*	y en donde <code>i</code> es el nivel actual del bfs.
+	*
+	**/
 	private String bfs(Grafo g,ArrayList<ArrayList<Integer>> cadenas){
 		Iterator<ArrayList<Integer>> recorrido_cadena = cadenas.iterator();
 		ArrayList<ArrayList<Integer>> siguientes_cadenas  = new ArrayList<ArrayList<Integer>>(cadenas);
-		int numero_cadenas_inicio = cadenas.size();
-		ArrayList<Integer> tamanos_inicio = this.get_Csize(cadenas);
-		int numero_cadenas_fin;
 		ArrayList<Integer> tamanos_fin;
 		ArrayList<Integer> cadena_actual;
-		int last,longitud_cadena,vecino_actual,posible_res;
 		HashSet<Integer> vecinos;
+		int last,longitud_cadena,vecino_actual,posible_res;
 
 		// primero buscamos el ultimo elemento de cada cadena:
 		while(recorrido_cadena.hasNext()){
@@ -56,26 +66,21 @@ public class BFS{
 					siguientes_cadenas.remove(cadena_actual);
 				}
 			}
-
 			// En este punto ya agregamos todos los adyacentes elegibles para una cadena
-			
-
 		}
-		numero_cadenas_fin = siguientes_cadenas.size();
+
+		// Para ver si un camino es hamiltoniano, como estamos construyendo puros caminos
+		// elementales (puesto que siempre agregamos un adyacente que no se repita en la cadena).
+		// basta con ver que la longitud de la cadena sea igual a la cardinalidad de los vertices.
+
+		// Sacamos la longitud a cada uno de los caminos.
 		tamanos_fin =  this.get_Csize(siguientes_cadenas);
+		// Vemos si alguna longitud es n.
 		posible_res = tamanos_fin.indexOf(g.get_vertices().size());
 
-		
 		if (posible_res!= -1){
-			String respuesta="";
-			
-			ArrayList<Integer> k = siguientes_cadenas.get(posible_res);
-			for (int j=0;j<g.get_dim().get(0);j++){
-				respuesta = respuesta + Integer.toString(k.get(j)) + " - ";
-			}
-			return respuesta;
-			
-			//return Arrays.toString(cadenas.get(posible_res) );
+			ArrayList<Integer> respuesta = siguientes_cadenas.get(posible_res);
+			return respuesta.toString();
 		}
 
 		if(siguientes_cadenas.equals(cadenas)){
@@ -87,10 +92,19 @@ public class BFS{
 	}
 
 
+	/** Funcion auxiliar que, dado una lista de caminos, retorna una lista
+	*	cuyos elementos son la longitud de cada camino.
+	*
+	*	@param cadena Lista de caminos a la cual se le sacara la longitud.
+	*
+	**/
 	private ArrayList<Integer> get_Csize(ArrayList<ArrayList<Integer>> cadena){
+
+		// creamos una lista de tamanos.
 		ArrayList<Integer> tamanos = new ArrayList<Integer>(cadena.size());
 		Iterator<ArrayList<Integer>> i = cadena.iterator();
 
+		// Y la llenamos con los tamanos de cada una de las cadenas.
 		while(i.hasNext()){
 			tamanos.add(i.next().size());
 		}
